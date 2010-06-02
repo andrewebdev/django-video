22587a1b59c56510e34546cdd970b3133f22adf0
Andre Engelbrecht <andre@teh-node.co.za>
2010-06-02 10:48:06 +0100

    Packaged django-video using distutils

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="videostream",
+    version="0.2",
+    url="http://github.com/andrewebdev/django-video",
+    description="A simple video streaming application for django",
+    author="Andre Engelbrech",
+    author_email="andre@teh-node.co.za",
+    packages=['videostream'],
+)
