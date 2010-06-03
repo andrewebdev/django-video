b72ba5ee852ecdb816cb1dbc3084789124d7458a
Andre Engelbrecht <andre@teh-node.co.za>
2010-06-03 16:16:12 +0100

    Removed copyright text in urls.py, since we have this in the main package folder anyway as LICENCE

--- src/videostream/urls.py
+++ src/videostream/urls.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# © Copyright 2009 Andre Engelbrecht. All Rights Reserved.
-# This script is licensed under the BSD Open Source Licence
-# Please see the text file LICENCE for more information
-# If this script is distributed, it must be accompanied by the Licence
-
 from django.conf.urls.defaults import *
 from videostream.models import *
 #from videostream.feeds import LatestStream
